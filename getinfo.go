57b9c46324beb2d17e25f194fb24c08c6b521343
David <davedogg323@gmail.com>
2022-05-10 11:22:37 -0700

    Scraping concurrently works

--- getinfo.go
+++ getinfo.go
@@ -10,47 +10,31 @@ import (
 //type House struct {
 
 //}
+func scrape(link string) {
+	c := colly.NewCollector()
 
+	// On every a element which has href attribute call callback
+	//var inStock bool
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		// Print link
+		fmt.Println(e.Text)
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit(link)
+
+}
 func main() {
-	go func(msg string) {
-		c := colly.NewCollector()
 
-		// On every a element which has href attribute call callback
-		//var inStock bool
+	go scrape("https://www.linkedin.com/jobs/entry-level-software-engineer-jobs/")
 
-		c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-			// Print link
-			fmt.Println(e.Text)
-		})
-
-		// Before making a request print "Visiting ..."
-		c.OnRequest(func(r *colly.Request) {
-			fmt.Println("Visiting", r.URL.String())
-		})
-
-		// Start scraping on https://hackerspaces.org
-		c.Visit("https://www.linkedin.com/jobs/entry-level-software-engineer-jobs/")
-
-		c2 := colly.NewCollector()
-
-		// On every a element which has href attribute call callback
-		//var inStock bool
-
-		c2.OnHTML("a[href]", func(e *colly.HTMLElement) {
-			// Print link
-			fmt.Println(e.Text)
-		})
-
-		// Before making a request print "Visiting ..."
-		c2.OnRequest(func(r *colly.Request) {
-			fmt.Println("Visiting", r.URL.String())
-		})
-
-		// Start scraping on https://hackerspaces.org
-		c2.Visit("https://www.indeed.com/q-Entry-Level-Software-Engineer-jobs.html?vjk=74a4204805ec6555")
-	}("going")
-
+	go scrape("https://www.indeed.com/q-Entry-Level-Software-Engineer-jobs.html?vjk=74a4204805ec6555")
 	time.Sleep(time.Second)
 	fmt.Println("done")
-
 }
