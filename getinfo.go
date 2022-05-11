e742b238cd0523255cc79dab56d96577387f95ff
David <davedogg323@gmail.com>
2022-05-11 16:23:27 -0700

    Three websites are able to be scraped from user input

--- getinfo.go
+++ getinfo.go
@@ -7,34 +7,48 @@ import (
 	"github.com/gocolly/colly"
 )
 
-//type House struct {
-
-//}
 func scrape(link string) {
+
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	//var inStock bool
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		// Print link
 		fmt.Println(e.Text)
 	})
 
-	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
 	c.Visit(link)
 
 }
 func main() {
+	var websites []string
 
-	go scrape("https://www.linkedin.com/jobs/entry-level-software-engineer-jobs/")
+	for i := 0; i < 3; i++ {
+		// Taking input from user
+		//reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter a job board to scrape: ")
+		var jobBoard string
 
-	go scrape("https://www.indeed.com/q-Entry-Level-Software-Engineer-jobs.html?vjk=74a4204805ec6555")
+		// Taking input from user
+		fmt.Scanln(&jobBoard)
+
+		websites = append(websites, jobBoard)
+
+	}
+
+	//https://www.linkedin.com/jobs/entry-level-software-engineer-jobs/
+	//https://www.indeed.com/q-Entry-Level-Software-Engineer-jobs.html?vjk=74a4204805ec6555
+
+	go scrape(websites[0])
+	go scrape(websites[1])
+	go scrape(websites[2])
 	time.Sleep(time.Second)
+
 	fmt.Println("done")
+
 }
