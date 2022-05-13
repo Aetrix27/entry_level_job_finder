07315b179d51a23eb37ee4a74e35c6967087348a
David <davedogg323@gmail.com>
2022-05-12 18:00:03 -0700

    Fixed bugs

--- getinfo.go
+++ getinfo.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/gocolly/colly"
 )
 
-func scrape(link string) {
-
-	c := colly.NewCollector()
+func scrape(link string, c *colly.Collector) {
 
 	// On every a element which has href attribute call callback
-
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		// Print link
-		fmt.Println(e.Text)
+		output := strings.Fields(e.Text)
+		for i := 0; i < len(output); i++ {
+			if output[i] == "Entry" || output[i] == "entry" {
+				if i+8 < len(output) {
+					fmt.Println(output[i : i+7])
+				}
+			}
+
+		}
+
 	})
 
 	c.OnRequest(func(r *colly.Request) {
@@ -25,10 +34,18 @@ func scrape(link string) {
 	c.Visit(link)
 
 }
+
 func main() {
 	var websites []string
+	c := colly.NewCollector()
+	option := 0
+	count := 0
+	add_another := ""
 
-	for i := 0; i < 3; i++ {
+	fmt.Println("Welcome to the Entry Level Job Board Scraper! Please enter some job boards: ")
+	for option < 1 {
+		reader := bufio.NewReader(os.Stdin)
+
 		// Taking input from user
 		//reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter a job board to scrape: ")
@@ -39,16 +56,27 @@ func main() {
 
 		websites = append(websites, jobBoard)
 
+		fmt.Print("Add another page? ")
+		add_another, _ = reader.ReadString('\n')
+		input2 := strings.TrimSpace(add_another)
+		fmt.Print(add_another)
+		count += 1
+		if input2 == "yes" {
+			continue
+		} else if input2 == "no" {
+			option = 1
+			break
+		}
+
 	}
 
-	//https://www.linkedin.com/jobs/entry-level-software-engineer-jobs/
+	//https://www.simplyhired.com/search?q=entry+level+software+engineer&l=los+angeles%2C+ca&job=3C1Bn0XyBJz10GUVBrsWKVgDDJ5wWIARRt-44vek8cIlDM_RRh7zZg
 	//https://www.indeed.com/q-Entry-Level-Software-Engineer-jobs.html?vjk=74a4204805ec6555
 
-	go scrape(websites[0])
-	go scrape(websites[1])
-	go scrape(websites[2])
-	time.Sleep(time.Second)
+	for i := 0; i < count; i++ {
 
-	fmt.Println("done")
+		scrape(websites[i], c)
+		time.Sleep(time.Second * 3)
+	}
 
 }
